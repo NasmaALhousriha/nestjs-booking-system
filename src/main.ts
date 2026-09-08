6e873f481f7fe985dead92b836e686383957631f
NasmaNas1 <nasmaaina@gmail.com>
2026-09-09 00:33:52 +0300

    ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    snapshot: false, // لإيقاف رسائل التحليلات المزعجة
+    snapshot: false, 
   });
 
-  // تفعيل الـ CORS لربط الفرونت إند بسلاسة
-  app.enableCors();
 
-  // إعدادات التوثيق (Swagger)
+  app.enableCors();
+app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, 
+      transform: true, 
+    }),
+  );
+  
   const config = new DocumentBuilder()
     .setTitle('Nasma API Documentation')
     .setDescription('The API description for frontend developers')
@@ -18,7 +24,7 @@ async function bootstrap() {
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // الرابط سيكون /api
+  SwaggerModule.setup('api', app, document); 
 
   await app.listen(3000);
 }
