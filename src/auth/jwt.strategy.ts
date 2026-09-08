2d151c818f3319b6ebc3d4a30ecc78ce14009839
NasmaNas1 <nasmaaina@gmail.com>
2026-09-09 00:21:05 +0300

    auth

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,15 +1,16 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
-    super({
+  constructor(private configService: ConfigService){
+   super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'YOUR_SECRET_KEY', 
-    });
+      secretOrKey: configService.get<string>('JWT_SECRET')!,
+       });
   }
 
   async validate(payload: any) {
